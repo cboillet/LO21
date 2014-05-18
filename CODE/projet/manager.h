4d098167affb1597bf1e8b42520fc02b75276c3c
cboillet <camille.boillet@gmail.com>
2014-05-18 14:32:13 +0200

    Dernière modif typename

--- CODE/projet/manager.h
+++ CODE/projet/manager.h
@@ -20,7 +20,7 @@
 
 using namespace std;
 
-template<class T>
+template<typename T>
 class Manager{
  private:
     T** t;
@@ -28,11 +28,11 @@ class Manager{
     unsigned int nbMax;
     void addItem(T* t);
     bool modification;
-    T* trouver(const QString& c) const=0;
+    T* trouver<T>(const QString& c) const=0;
     Manager(const Manager& m);
     Manager& operator=(const Manager& m);
     Manager():t(0),nb(0),nbMax(0),file(""),modification(false){}
-    ~Manager()=0;
+    virtual ~Manager();
     QString file;
     friend struct Handler;
     struct Handler{
@@ -43,14 +43,7 @@ class Manager{
     static Handler handler;
 
  public:
-
-    void load(const QString& f)=0;
-    void save(const QString& f)=0;
-    static Manager& getInstance();
     static void libererInstance();
-    //void ajouterUV(const QString& c, const QString& t, unsigned int nbc, Categorie cat, bool a, bool p);
-    const T& get(const QString& code) const=0;
-    T& get(const QString& code)=0; //marche pour UV et Cursus car on tout les deux un attributs noms
     class Iterator {
         friend class Manager;
         T** currentT;
@@ -87,38 +80,72 @@ class Manager{
     };
     iterator begin() { return iterator(t); }
     iterator end() { return iterator(t+nb); }
-
-   /* class FilterIterator {
-        friend class UVManager;
-        UV** currentUV;
-        unsigned int nbRemain;
-        Categorie categorie;
-        FilterIterator(UV** u, unsigned nb, Categorie c):currentUV(u),nbRemain(nb),categorie(c){
-            while(nbRemain>0 && (*currentUV)->getCategorie()!=categorie){
-                nbRemain--; currentUV++;
-            }
-        }
-    public:
-        FilterIterator():nbRemain(0),currentUV(0){}
-        bool isDone() const { return nbRemain==0; }
-        void next() {
-            if (isDone())
-                throw UTProfilerException("error, next on an iterator which is done");
-            do {
-                nbRemain--; currentUV++;
-            }while(nbRemain>0 && (*currentUV)->getCategorie()!=categorie);
-        }
-        UV& current() const {
-            if (isDone())
-                throw UTProfilerException("error, indirection on an iterator which is done");
-            return **currentUV;
-        }
-    };
-    FilterIterator getFilterIterator(Categorie c) {
-        return FilterIterator(uvs,nbUV,c);
-    }
-    */
 };
 
+template <typename T> class StrategieUv<T>{
+    public:
+    void load(Manager<T>& man, const QString& f)=0;
+    void save(Manager<T>& man, const QString& f)=0;
+    void ajouterUV(Manager<T>&, const QString& c, const QString& t, unsigned int nbc, Categorie cat, bool a, bool p);
+};
+
+template <typename T> class StrategieUvXML<T>: public StrategieUv{
+    public:
+    void load(Manager<T>& man, const QString& f){}
+    void save(Manager<T>& man, const QString& f){}
+    void ajouterUV(Manager<T>&, const QString& c, const QString& t, unsigned int nbc, Categorie cat, bool a, bool p){}
+};
+template <typename T> class StrategieUvSQL<T>: public StrategieUv{
+    public:
+    void load(Manager<T>& man, const QString& f){=0;}
+    void save(Manager<T>& man, const QString& f){=0;}
+    void ajouterUV(Manager<T>&, const QString& c, const QString& t, unsigned int nbc, Categorie cat, bool a, bool p){}
+};
+
+template <typename T>
+class UVManager: public Manager<T>{
+    private:
+        StrategieUV stratUV;
+        UVManager():Manager<T>(),stratUV(StrategieXML){}
+        UV* trouver<T>(const QString& c) const; //peut être mettre T en paramètre
+        ~Manager();
+
+    public:
+        void load(const StrategieUv& stategie, const QString& f){strat.load(*this,const QString& f);}
+        void save(const StrategieUv& strategieSave, const QString& f){strat.save(*this,const QString& f);}
+        void ajouter<T>(const Strategie& strat,const QString& c, const QString& t, unsigned int nbc, Categorie cat, bool a, bool p) {strat.ajouterUV(*this,const QString& f);}
+        UV& get<T>(const Strategie& strat, const QString& code);
+        const UV& get<T>(const QString& code) const;
+
+        class FilterIterator {
+             friend class UVManager;
+             UV** currentUV;
+             unsigned int nbRemain;
+             Categorie categorie;
+             FilterIterator(UV** u, unsigned nb, Categorie c):currentUV(u),nbRemain(nb),categorie(c){
+                 while(nbRemain>0 && (*currentUV)->getCategorie()!=categorie){
+                     nbRemain--; currentUV++;
+                 }
+             }
+         public:
+             FilterIterator():nbRemain(0),currentUV(0){}
+             bool isDone() const { return nbRemain==0; }
+             void next() {
+                 if (isDone())
+                     throw UTProfilerException("error, next on an iterator which is done");
+                 do {
+                     nbRemain--; currentUV++;
+                 }while(nbRemain>0 && (*currentUV)->getCategorie()!=categorie);
+             }
+             UV& current() const {
+                 if (isDone())
+                     throw UTProfilerException("error, indirection on an iterator which is done");
+                 return **currentUV;
+             }
+         };
+         FilterIterator getFilterIterator(Categorie c) {
+             return FilterIterator(uvs,nbUV,c);
+         }
+};
 
 #endif // MANAGER_H
